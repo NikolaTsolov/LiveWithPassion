15b0f415873d3167de9fce5a92a179b21e5bc904
DESKTOP-93BFT1O\Nikola Tsolov <nikola.tsolov@gmail.com>
2016-12-16 14:37:15 +0200

    Leaving the thing only for BackEnd

--- src/LiveWithPassion/live_with_passion/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class LiveWithPassionConfig(AppConfig):
-    name = 'live_with_passion'
--- src/LiveWithPassion/live_with_passion/models.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-from django.conf import settings
-
-from .validators import validate_email
-# Create your models here.
-
-
